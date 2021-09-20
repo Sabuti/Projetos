76dbf0c5bd9e700c99a03d2f746653ce74470621
Lucas Linhares <81273468+Sabuti@users.noreply.github.com>
2021-09-20 11:10:04 -0300

    Update abrir_aula_black.py

--- abrir_aula_black.py
+++ abrir_aula_black.py
@@ -10,7 +10,6 @@ class Entra_Aula:
         usa.click(x=523, y=1053)
         time.sleep(5)
         usa.hotkey('ctrlleft','t')
-        usa.write('https://ava.pucpr.br/blackboardauth/')
         usa.press('enter')
         time.sleep(4)
         usa.press('enter')
