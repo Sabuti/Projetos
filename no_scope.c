fea69744d344e93dbbc717c7431dfe291d56c858
Lucas Linhares <81273468+Sabuti@users.noreply.github.com>
2022-08-10 23:30:32 -0300

    Range from User Input

---
+++ no_scope.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int pao;
+    int i;
+    int escolha1, escolha2;
+    printf("De quanto vc quer sair? ");
+    scanf("%i", &escolha1);
+    printf("De quanto vc quer chegar? ");
+    scanf("%i", &escolha2);
+    for (i=escolha1; i<escolha2; i++){
+        printf("%i \n", i);
+    }
+    return 0;
+}
